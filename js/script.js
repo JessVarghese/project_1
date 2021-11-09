952922c79bb2928098fcad5339c06dd01b3f459b
Abeer Tannous <{abeer.tannous}@yahoo.com>
2021-11-09 10:24:01 -0500

    fetch code

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/vnd.api+json");
+myHeaders.append("Authorization", "94css0vo");
+
+var raw = JSON.stringify({
+  "data": {
+    "filterRadius": {
+      "miles": 100,
+      "coordinates": "34.1031,-118.416"
+    }
+  }
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow',
+  limit:5
+};
+
+fetch("https://api.rescuegroups.org/v5/public/animals/search/available/dogs/haspic?fields[animals]=distance&include=breeds,colors,orgs,patterns,pictures,species&sort=random&limit=1", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+console.log(response.data[0]);
+
+
+
+
+
+
+
+
