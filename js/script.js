89d24063146dda4171768ab286735a1bf3d785b2
Jess Varghese <jessvarghese@gmail.com>
2021-11-08 21:11:33 -0500

    added radio buttons

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/vnd.api+json");
+myHeaders.append("Authorization", "94css0vo");
+
+var raw = JSON.stringify({
+  "data": {
+    "filterRadius": {
+      "miles": 25,
+      "postalcode": 90210
+    }
+  }
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+
+    fetch("https://api.rescuegroups.org/v5/public/animals/search/available/haspic?fields[animals]=distance&include=breeds,locations&sort=random&limit=1", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+document.querySelector("results").innerHTML = result.data;
+    
+
+
+
+
+ 
+
